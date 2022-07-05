1cde7c67745bc212608f8a9f3c8183a4254d5b63
Kenneth Abrefa <kenn@fb.com>
2022-07-05 11:55:40 -0700

    Book detail page

--- app/src/main/java/com/example/bookclub/BookDetailsActivity.java
+++ app/src/main/java/com/example/bookclub/BookDetailsActivity.java
@@ -1,6 +1,7 @@
 package com.example.bookclub;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
