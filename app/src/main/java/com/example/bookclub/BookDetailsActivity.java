2c11eda3c3a2117dcbca0a79d12ac0c667d23916
Kenneth Abrefa <kenn@fb.com>
2022-07-05 13:50:15 -0700

    Book detail page

--- app/src/main/java/com/example/bookclub/BookDetailsActivity.java
+++ app/src/main/java/com/example/bookclub/BookDetailsActivity.java
@@ -10,8 +10,7 @@ import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.SyncStateContract;
-import android.support.v4.content.FileProvider;
+
 import android.text.TextUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
